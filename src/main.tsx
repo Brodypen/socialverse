07cb24b6b206927b6e56a13fe9ae33a817ec87f0
Brodypen <brodypen7987@gmail.com>
2023-06-11 18:45:01 -0500

    Reached checkpoint 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddCreator from "./pages/AddCreator.tsx";
+import EditCreator from "./pages/EditCreator.tsx";
+import ViewCreator from "./pages/ViewCreator.tsx";
+import ShowCreators from "./pages/ShowCreators.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/add-creator" element={<AddCreator />} />
+        <Route path="/edit-creator" element={<EditCreator />} />
+        <Route path="/view-creator" element={<ViewCreator />} />
+        <Route path="/show-creators" element={<ShowCreators />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
