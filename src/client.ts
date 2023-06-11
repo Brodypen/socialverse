5759ba5720d3e21eff88ca217fc3f17a10fd46e5
Brodypen <brodypen7987@gmail.com>
2023-06-11 18:31:07 -0500

    Finish Step 1

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
-const URL = process.env.URL;
-const API_KEY = process.env.API_KEY;
+const URL= `${process.env.URL}`;
+const API_KEY = `${process.env.API_KEY}`;
+
+export const supabase = createClient(URL, API_KEY);
