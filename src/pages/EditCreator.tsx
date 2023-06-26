d311ac7f7d63a19b2fb8d7807bd5da7b1c66fbbe
Brodypen <brodypen7987@gmail.com>
2023-06-26 09:46:30 -0500

    feat ViewCreator

--- src/pages/EditCreator.tsx
+++ src/pages/EditCreator.tsx
@@ -1,9 +1,148 @@
-import React from 'react'
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import Header from "../components/Header";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../components/ui/form";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+const formSchema = z.object({
+  name: z.string().min(1).max(50),
+  imageURL: z.string().url(),
+  url: z.string().url(),
+  description: z.string().min(1).max(1000),
+});
+import { supabase } from "../client";
 
 const EditCreator = () => {
-  return (
-    <div>EditCreator</div>
-  )
-}
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-export default EditCreator
+  const onSubmit = async (info: z.infer<typeof formSchema>) => {
+    const { error } = await supabase
+      .from("creators")
+      .update({
+        name: info.name,
+        imageURL: info.imageURL,
+        description: info.description,
+        url: info.url,
+      })
+      .eq("id", id);
+
+    if (error) {
+      console.log(error);
+    } else {
+      navigate("/");
+    }
+  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageURL: "",
+      description: "",
+      url: "",
+    },
+  });
+
+  useEffect(() => {
+    const fetchCreator = async () => {
+      const { data, error } = await supabase
+        .from("creators")
+        .select()
+        .eq("id", id)
+        .single();
+      if (error) {
+        // TODO: Handle error, change to 404 page
+        navigate("/");
+      }
+      if (data) {
+        form.reset(data);
+      }
+    };
+    fetchCreator();
+  }, [id, navigate, form]);
+
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Button onClick={() => navigate("/")}>Go back</Button>
+
+      <div className="pt-5">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="imageURL"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>ImageURL</FormLabel>
+                  <FormControl>
+                    <Input placeholder="https://" {...field} />
+                  </FormControl>
+                  <FormDescription>Profile Pic Image URL</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="url"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Social url</FormLabel>
+                  <FormControl>
+                    <Input placeholder="https://www.youtube.com/" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Link to their main platform!
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Creator is a x..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default EditCreator;
--- src/pages/ViewCreator.tsx
+++ src/pages/ViewCreator.tsx
@@ -1,152 +1,41 @@
+import React from 'react'
 import { useParams, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Header from "../components/Header";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "../components/ui/form";
-import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input";
-const formSchema = z.object({
-  name: z.string().min(1).max(50),
-  imageURL: z.string().url(),
-  url: z.string().url(),
-  description: z.string().min(1).max(1000),
-});
+import { useEffect } from "react";
 import { supabase } from "../client";
-import { CreatorType } from "../types/collection";
-
+import { CreatorType } from '../types/collection';
+import Header from '../components/Header';
+import { Button } from '../components/ui/button';
 const ViewCreator = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [creator, setCreator] = useState<CreatorType>();
-
-  const onSubmit = async (info: z.infer<typeof formSchema>) => {
-    const { data, error } = await supabase.from("creators").update(
-      {
-        name: info.name,
-        imageURL: info.imageURL,
-        description: info.description,
-        url: info.url,
-      }).eq("id", id)
-
-    if (error) {
-      console.log(error);
-    } else {
-      navigate("/");
-    }
-  };
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        name: "",
-        imageURL: "",
-        description: "",
-        url: "",
-      },
-    });
+  const [creator, setCreator] = React.useState<CreatorType>();
 
   useEffect(() => {
     const fetchCreator = async () => {
       const { data, error } = await supabase
         .from("creators")
         .select()
-        .eq("id", id).single();
+        .eq("id", id)
+        .single();
       if (error) {
         // TODO: Handle error, change to 404 page
         navigate("/");
       }
       if (data) {
-        // console.log(data)
         setCreator(data);
-        form.reset(data);
       }
     };
     fetchCreator();
-    console.log("fetched again?")
   }, [id, navigate]);
 
-
-
   return (
-    <div className="min-h-screen">
+    <div>
       <Header />
       <Button onClick={() => navigate("/")}>Go back</Button>
-
-      <div className="pt-5">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="imageURL"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>ImageURL</FormLabel>
-                  <FormControl>
-                    <Input placeholder="https://" {...field} />
-                  </FormControl>
-                  <FormDescription>Profile Pic Image URL</FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="url"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Social url</FormLabel>
-                  <FormControl>
-                    <Input placeholder="https://www.youtube.com/" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    Link to their main platform!
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Creator is a x..." {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
-      </div>
+      
+      ViewCreator - {id} - {creator?.name}
     </div>
   );
-};
+}
 
-export default ViewCreator;
+export default ViewCreator
